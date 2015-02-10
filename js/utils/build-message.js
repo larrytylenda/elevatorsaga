c3a74cee7f067fdcadd11174258f21109a728e53
larrytylenda <larrytylenda@outlook.com>
2015-02-10 09:37:50 -0500

    Create build-message.js

---
+++ js/utils/build-message.js
@@ -0,0 +1,12 @@
+/* global define */
+define(function (require) {
+  'use strict';
+  
+  var buildMessage = function (method, message) {
+    return function (variable, value) {
+      console.log(method + ' ' + message + ' - ' + variable);
+      console.log(value);
+    };
+  };
+  return buildMessage;
+});
